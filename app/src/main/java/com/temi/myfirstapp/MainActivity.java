56f18c638739f9285d46a9d5617b75a69f33e947
ErvinK123 <ervinkin@yahoo.com.sg>
2023-05-10 13:18:12 +0800

    Add 4 buttons with turning functionality.

--- app/src/main/java/com/temi/myfirstapp/MainActivity.java
+++ app/src/main/java/com/temi/myfirstapp/MainActivity.java
@@ -5,18 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
 public class MainActivity extends AppCompatActivity implements OnRobotReadyListener {
 
+    Button btnFront, btnRight, btnLeft, btnBack;
     private Robot robot = Robot.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFront = findViewById(R.id.button1);
+        btnRight = findViewById(R.id.button2);
+        btnLeft = findViewById(R.id.button3);
+        btnBack = findViewById(R.id.button4);
+
+        btnFront.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //robot.skidJoy(1,0, true);
+            }
+        });
+
+        btnRight.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                robot.turnBy(90);
+                //robot.skidJoy(1,0, true);
+            }
+        });
+
+        btnLeft.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                robot.turnBy(-90);
+                //robot.skidJoy(1,0,true);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                robot.turnBy(180);
+                //robot.skidJoy(1,0,true);
+            }
+        });
     }
 
     protected void onStart() {
